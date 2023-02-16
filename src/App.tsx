1d7e143d2248c13691e34ca189b1b36136c0e77e
Martynas Gircys <mgircys@rocketsoftware.com>
2023-02-16 22:56:51 +0200

    Add types for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
 import "./index.css";
+import openJson from "../secretData/stockRecs/stockAdv/open.json";
+
+interface StockRecommendations {
+  portfolios: string[]; // e.g. ["@StockAdvisorTomInclusion", "@StockAdvisorDavidInclusion"]
+  recs: StockRecommendation[];
+}
+
+interface StockRecommendation {
+  returns: number; // e.g. 0.058763 // ---> not sure if dividend included
+  open_date: string; // e.g. "2023-02-02T00:00:00"
+  ticker_symbol: string;
+}
+
+const openRecommendations = openJson as StockRecommendations;
 
 function App() {
   return <>Oh, hello! ✨</>;
